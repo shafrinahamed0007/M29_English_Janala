a2bab8f7b5a9be3ac77767fa5d75a5a3a7f2a0f3
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-04 04:15:05 +0600

    Load and display all leasons with API as figma

---
+++ script/index.js
@@ -0,0 +1,30 @@
+const loadLessons = () => {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+};
+
+const displayLesson = (lessons) => {
+ //   1. get the container & Empty
+  const levelContainer = document.getElementById("levelContainer");
+  levelContainer.innerHTML = "";
+  // 2. get into every lessons
+  for (let lesson of lessons) {
+    //   3. create element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+     <button class = "btn btn-outline btn-primary">
+       <i class="fa-solid fa-book-open"></i>
+       Lesson -  ${lesson.level_no}
+       </button>
+    
+    
+    `;
+    // 4. append into container
+    levelContainer.append(btnDiv);
+  }
+};
+
+loadLessons();
