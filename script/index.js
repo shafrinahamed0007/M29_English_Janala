add40a5d431816536089a58ed8fc7b69169afc21
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-04 14:28:31 +0600

    Load Words By Level And Display Card

--- script/index.js
+++ script/index.js
@@ -7,7 +7,7 @@ const loadLessons = () => {
 };
 
 const displayLesson = (lessons) => {
- //   1. get the container & Empty
+  //   1. get the container & Empty
   const levelContainer = document.getElementById("levelContainer");
   levelContainer.innerHTML = "";
   // 2. get into every lessons
@@ -15,9 +15,9 @@ const displayLesson = (lessons) => {
     //   3. create element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-     <button class = "btn btn-outline btn-primary">
+     <button onclick = "loadLevelWord(${lesson.level_no})"  class = "btn btn-outline btn-primary">
        <i class="fa-solid fa-book-open"></i>
-       Lesson -  ${lesson.level_no}
+       Lesson - ${lesson.level_no}
        </button>
     
     
@@ -25,6 +25,35 @@ const displayLesson = (lessons) => {
     // 4. append into container
     levelContainer.append(btnDiv);
   }
+};
+
+const loadLevelWord = (id) => {
+  // console.log(id)
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  // console.log(url)
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  // console.log(words);
+  const wordContainer = document.getElementById("wordContainer");
+  wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    // console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+     <div class="bg-white rounded-xl shadow-sm text-center py-20 px-5">
+                <h2 class="font-bold ">${word.word}</h2>
+                <p class="my-6">Meaning /Pronounciation</p>
+                <p class="font-bangla">"${word.meaning} / ${word.pronunciation}"</p>
+            </div>
+    `;
+    wordContainer.append(card);
+  });
 };
 
 loadLessons();
